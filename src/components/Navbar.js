b237ecb1b430947ed6cc8049d0070616ed670d9b
Hotmonk <avinashkhetri@gmail.com>
2019-12-30 11:06:05 +0530

    data file and cards added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,33 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import logo from '../homeLogo.svg'
-import cartLogo from '../cartLogo.svg'
-import styled from 'styled-components'
-export default class Navbar extends Component{
-    render(){
-        return(
-            <nav class="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
-                <Link to='/'>
-                    <img src={logo} alt="store" classNamw="navbar-brand"/>
-                </Link>
-                <ul className="navbar-nav align-items-center">
-                    <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">
-                            Products
-                        </Link>
-                    </li>
-                </ul>
-                <Link to='/cart' className="ml-auto">
-                    <ButtonContainer>
-                        <span className="mr-2">
-                            <img src={cartLogo} alt="cart"/>
-                        </span>
-                    My cart
-                    </ButtonContainer>
-                </Link>
-            </nav>
-        )
-    }
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../homeLogo.svg";
+import cartLogo from "../cartLogo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav style={{backgroundColor:'#2a2a72'}} class="navbar navbar-expand-sm  navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <span className="mr-2">
+              <img src={cartLogo} alt="cart" />
+            </span>
+            My cart
+          </ButtonContainer>
+        </Link>
+      </nav>
+    );
+  }
 }
 
-const ButtonContainer= styled.button`
-text-transform: capitalize;
-font-size:1.4rem;
-background: transparent;
-border:0.05rem solid var(--lightBlue);
-color:var(--lightBue);
-border-radius:0.5rem;
-padding: 0.2rem 0.5rem 0.2rem 0;
-cursor:pointer;
-transition:all 0.5s ease-in-out;
-&:hover{
-    background:var(--lightBlue);
-    color:var(--mainBlue);
-}
-&:focus{
-    outline: none;
-}
-`;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import cartLogo from "../cartLogo.svg";
+import {ProductConsumer} from "../context";
+
 
 export default class Product extends Component {
     render() {
+        const {id, title, img, price, inCart}= this.props.product;
         return (
-            <div>
-               <h3> Hello from Product</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+               <div className="card">
+                   <div className="img-container p5" onClick={()=>console.log('clicked')}>
+                       <Link to="/details">
+                           <img src={img} alt="product" className="card-img-top"/>
+                       </Link>
+                       <button className="cart-btn" dissabled={inCart?true:false} onClick={()=>{console.log('added to the cart');}}>
+                       {inCart?(<p className="text-capitalize mb-0" disabled>{" "}>In Cart</p>):(<img src={cartLogo} alt="cart" />)}
+                        </button>
+                   </div>
+               </div>
+            </ProductWrapper>
         )
     }
 }
+
+const ProductWrapper= styled.div`
+`
